d48660ac79e9d366ca4d5efefd7c0d251d6ddfbd
mohansathiamoorthy007-cyber <mohansathiamoorthy007@gmail.com>
2026-02-19 11:06:42 +0530

    Update listener.cpp

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,34 +1,19 @@
-#include <memory>
-#include <functional>
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-using std::placeholders::_1;
-
-class Listener : public rclcpp::Node
-{
-public:
-    Listener()
-        : Node("listener")
-    {
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
-            "/chatter", 10, std::bind(&Listener::topic_callback, this, _1));
-    }
-
-private:
-    void topic_callback(const std_msgs::msg::String::ConstSharedPtr msg) const
-    {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    }
-
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-};
-
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Listener>());
-    rclcpp::shutdown();
-    return 0;
-}
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="ros2_launch_demo",
+            executable="talker",
+            name="talker",
+            output="screen",
+            parameters=[{"message_prefix": "ROS2"}],
+        ),
+        Node(
+            package="ros2_launch_demo",
+            executable="listener",
+            name="listener",
+            output="screen",
+        ),
+    ])
