545072e5035a1c2095955d8c70e29a3be8dbc251
mohansathiamoorthy007-cyber <mohansathiamoorthy007@gmail.com>
2026-02-19 11:07:26 +0530

    Update talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,46 +1,24 @@
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+def generate_launch_description():
 
-using namespace std::chrono_literals;
+    talker_node = Node(
+        package="ros2_launch_demo",
+        executable="talker",
+        name="talker",
+        output="screen",
+        parameters=[{"message_prefix": "ROS2"}],  # overrides default "Hello"
+    )
 
-class Talker : public rclcpp::Node
-{
-public:
-    Talker()
-        : Node("talker"), count_(0)
-    {
-        // Declare parameter with default value
-        this->declare_parameter("message_prefix", "Hello");
+    listener_node = Node(
+        package="ros2_launch_demo",
+        executable="listener",
+        name="listener",
+        output="screen",
+    )
 
-        publisher_ = this->create_publisher<std_msgs::msg::String>("/chatter", 10);
-        timer_ = this->create_wall_timer(
-            1000ms, std::bind(&Talker::timer_callback, this));
-    }
-
-private:
-    void timer_callback()
-    {
-        std::string prefix = this->get_parameter("message_prefix").as_string();
-        auto message = std_msgs::msg::String();
-        message.data = prefix + ": " + std::to_string(count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        publisher_->publish(message);
-    }
-
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-};
-
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Talker>());
-    rclcpp::shutdown();
-    return 0;
-}
+    return LaunchDescription([
+        talker_node,
+        listener_node,
+    ])
